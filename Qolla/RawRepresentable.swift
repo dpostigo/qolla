55b5b4a22bc41cb7d68b951fc5266f7c301956a9
Daniela Postigo <dani.postigo@gmail.com>
2018-03-11 18:23:32 -0400

    titled protocol

--- Qolla/RawRepresentable.swift
+++ Qolla/RawRepresentable.swift
@@ -49,10 +49,28 @@ extension Sequence where Element: RawRepresentable, Element.RawValue == Int {
 }
 
 extension RawRepresentable where Self: Equatable {
-
 	public static func -(lhs: [Self], rhs: Self) -> [Self] {
 		return lhs - [rhs]
 	}
 }
 
 
+public protocol Titled {
+	var title: String { get }
+}
+
+extension Sequence where Element: Titled {
+	public var titles: [String] { return self.map { $0.title } }
+}
+
+extension RawRepresentable where RawValue == Int, Self: Titled {
+	
+	public init?(title: String) {
+		switch Self.titles.index(of: title) {
+			case .none: return nil
+			case .some(let index): self.init(rawValue: index)
+		}
+	}
+	
+	public static var titles: [String] { return self.all.titles }
+}
